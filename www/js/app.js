989dd2d95c7bb9e0d1444dfddcedaca77e9a4071
Damian Lajara <damiansito93@gmail.com>
2015-07-16 23:58:15 -0400

    added a side menu

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,16 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('menu', {
+      url: "/menu",
+      abstract: true,
+      templateUrl: "templates/menu.html",
+      controller: 'MenuCtrl'
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/menu');
+});
