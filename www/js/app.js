d1641e4efa650f55c255daf2565b9cd3e382cb11
Damian Lajara <damiansito93@gmail.com>
2015-07-18 19:06:04 -0400

    Created and finalized the about me view prototype

--- www/js/app.js
+++ www/js/app.js
@@ -3,16 +3,16 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','starter.controllers','ui.bootstrap','ngAnimate'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ui.bootstrap', 'ngAnimate'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
@@ -21,22 +21,22 @@ angular.module('starter', ['ionic','starter.controllers','ui.bootstrap','ngAnima
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-  .state('menu', {
+    .state('menu', {
       url: "/menu",
       abstract: true,
       templateUrl: "templates/menu.html",
       controller: 'MenuCtrl'
-  })
-  .state('menu.home', {
-     url: "/home",
-     views: {
-       'menuContent': {
-         templateUrl: "templates/home.html",
-         controller: 'HomeCtrl'
-       }
-     }
-   })
-   .state('menu.bookings', {
+    })
+      .state('menu.home', {
+        url: "/home",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/home.html",
+            controller: 'HomeCtrl'
+          }
+        }
+      })
+      .state('menu.bookings', {
         url: "/bookings",
         views: {
           'menuContent': {
@@ -45,33 +45,56 @@ angular.module('starter', ['ionic','starter.controllers','ui.bootstrap','ngAnima
           }
         }
       })
-      .state('menu.aboutUs', {
-        url: "/aboutUs",
-        views: {
-          'menuContent': {
-            templateUrl: "templates/aboutUs.html",
-            controller: 'AboutUsCtrl'
-          }
-        }
-      })
-   .state('menu.events', {
-      url: "/events",
+    .state('menu.aboutUs', {
+      url: "/aboutUs",
       views: {
         'menuContent': {
-          templateUrl: "templates/events.html",
-          controller: 'EventsCtrl'
+          templateUrl: "templates/aboutUs.html",
+          controller: 'AboutUsCtrl'
+        },
+        /* Angular ui-router absolute View Names */
+        // https://github.com/angular-ui/ui-router/wiki/Multiple-Named-Views
+        'tab-home@menu.aboutUs': {
+          templateUrl: 'templates/tabHome.html',
+          controller: 'TabHomeCtrl'
+        },
+        'tab-terms@menu.aboutUs': {
+          templateUrl: 'templates/tabTerms.html',
+          controller: 'TabTermsCtrl'
+        },
+        'tab-contact@menu.aboutUs': {
+          templateUrl: 'templates/tabContact.html',
+          controller: 'TabContactCtrl'
         }
       }
     })
-    .state('menu.detail', {
-    url: "/events/:eventId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/eventDetail.html",
-        controller: 'EventDetailCtrl'
-      }
-    }
-  });
+    .state('menu.terms', {
+        url: "/terms",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/tabTerms.html",
+            controller: 'TabTermsCtrl'
+          }
+        }
+      })
+    .state('menu.events', {
+        url: "/events",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/events.html",
+            controller: 'EventsCtrl'
+          }
+        }
+      })
+      .state('menu.detail', {
+        url: "/events/:eventId",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/eventDetail.html",
+            controller: 'EventDetailCtrl'
+          }
+        }
+      });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/menu/home');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,130 +1,140 @@
 angular.module('starter.controllers', [])
 
-.controller('MenuCtrl', function ($scope) {
+.config(function($ionicConfigProvider) {
+  $ionicConfigProvider.tabs.position('bottom');
+  $ionicConfigProvider.navBar.alignTitle('center')
+})
+
+.controller('MenuCtrl', function($scope) {
 
 })
 
-.controller('AboutUsCtrl', function ($scope) {
+.controller('TabHomeCtrl', function($scope) {
   $scope.myInterval = 5000;
-    var slides = $scope.slides = [];
-    $scope.addSlide = function() {
-      var newWidth = 600 + slides.length + 1;
-      slides.push({
-        image: 'http://placekitten.com/' + newWidth + '/300',
-        text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-          ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-      });
-    };
-    for (var i=0; i<4; i++) {
-      $scope.addSlide();
-    }
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: 'http://placekitten.com/' + newWidth + '/300',
+      text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i = 0; i < 4; i++) {
+    $scope.addSlide();
+  }
 })
 
-.controller('HomeCtrl', function ($scope,$sce) {
+.controller('TabTermsCtrl', function($scope) {})
+
+.controller('TabContactCtrl', function($scope) {
+
+})
+
+.controller('AboutUsCtrl', function($scope, $state) {
+
+})
+
+.controller('HomeCtrl', function($scope, $sce) {
   $scope.dynamicTooltip = 'Hello, World!';
-    $scope.dynamicTooltipText = 'dynamic';
-    $scope.htmlTooltip = $sce.trustAsHtml('I\'ve been made <b>bold</b>!');
+  $scope.dynamicTooltipText = 'dynamic';
+  $scope.htmlTooltip = $sce.trustAsHtml('I\'ve been made <b>bold</b>!');
 
 })
 
-.controller('BookingsCtrl', function ($scope) {
-   $scope.today = function() {
-     $scope.dt = new Date();
-   };
-   $scope.today();
+.controller('BookingsCtrl', function($scope) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
 
-   $scope.clear = function () {
-     $scope.dt = null;
-   };
+  $scope.clear = function() {
+    $scope.dt = null;
+  };
 
-   // Disable weekend selection
-   $scope.disabled = function(date, mode) {
-     return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-   };
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+  };
 
-   $scope.toggleMin = function() {
-     $scope.minDate = $scope.minDate ? null : new Date();
-   };
-   $scope.toggleMin();
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
 
-   $scope.open = function($event) {
-     $event.preventDefault();
-     $event.stopPropagation();
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
 
-     $scope.opened = true;
-   };
+    $scope.opened = true;
+  };
 
-   $scope.dateOptions = {
-     formatYear: 'yy',
-     startingDay: 1
-   };
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
 
-   $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-   $scope.format = $scope.formats[0];
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
 
-   var tomorrow = new Date();
-   tomorrow.setDate(tomorrow.getDate() + 1);
-   var afterTomorrow = new Date();
-   afterTomorrow.setDate(tomorrow.getDate() + 2);
-   $scope.events =
-     [
-       {
-         date: tomorrow,
-         status: 'full'
-       },
-       {
-         date: afterTomorrow,
-         status: 'partially'
-       }
-     ];
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  var afterTomorrow = new Date();
+  afterTomorrow.setDate(tomorrow.getDate() + 2);
+  $scope.events = [{
+    date: tomorrow,
+    status: 'full'
+  }, {
+    date: afterTomorrow,
+    status: 'partially'
+  }];
 
-   $scope.getDayClass = function(date, mode) {
-     if (mode === 'day') {
-       var dayToCheck = new Date(date).setHours(0,0,0,0);
+  $scope.getDayClass = function(date, mode) {
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
 
-       for (var i=0;i<$scope.events.length;i++){
-         var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+      for (var i = 0; i < $scope.events.length; i++) {
+        var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
 
-         if (dayToCheck === currentDay) {
-           return $scope.events[i].status;
-         }
-       }
-     }
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
+        }
+      }
+    }
 
-     return '';
-   };
+    return '';
+  };
 })
 
-.controller('EventsCtrl', function ($scope, JsonEvents) {
+.controller('EventsCtrl', function($scope, JsonEvents) {
 
-  JsonEvents.retrieveData().then(function(httpObj){
+  JsonEvents.retrieveData().then(function(httpObj) {
     $scope.events = httpObj.data;
   });
 })
 
-.controller('EventDetailCtrl', function ($scope, $stateParams, JsonEvents) {
-  JsonEvents.retrieveData().then(function(httpObj){
+.controller('EventDetailCtrl', function($scope, $stateParams, JsonEvents) {
+  JsonEvents.retrieveData().then(function(httpObj) {
     var events = httpObj.data;
     //Simple id lookup
     for (var i = 0; i < events.length; i++) {
-       if (events[i].id == $stateParams.eventId) {
-         $scope.event = events[i];
-         break;
-       }
-     }
+      if (events[i].id == $stateParams.eventId) {
+        $scope.event = events[i];
+        break;
+      }
+    }
   });
 })
 
 //$http returns a promise anyway so you don't need $q
-.factory('JsonEvents', function ($http){
+.factory('JsonEvents', function($http) {
   return {
-    retrieveData : function() {
+    retrieveData: function() {
       return $http
         .get('js/events.json')
         .success(function(data) {
           console.log("Success in parsing json!", data);
         })
-        .error(function (status) {
+        .error(function(status) {
           console.log("Error in parsing json!");
         });
     }
